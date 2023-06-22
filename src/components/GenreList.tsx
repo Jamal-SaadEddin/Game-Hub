48ee1da9f946e516ee1bf5c0386635be0bad28a3
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-22 15:12:55 +0300

    Show genre loading skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImage from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) return <GenreListSkeleton />;
 
   return (
     <List>
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,27 @@
+import {
+  HStack,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+
+const GameListSkeleton = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
+  return (
+    <List>
+      {skeletons.map((skeleton) => (
+        <ListItem key={skeleton} paddingY="5px">
+          <HStack>
+            <SkeletonCircle />
+            <SkeletonText noOfLines={1} width="100px" />
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GameListSkeleton;
