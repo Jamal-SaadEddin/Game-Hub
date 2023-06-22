dab538a4c699587a2e06322b306f95f9e5a5e584
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-22 12:03:30 +0300

    Displaying Critic Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
